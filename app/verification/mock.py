699af0e091999171d15240279c33f0feb804e3c7
Ruben Horn <horn.ruben@yahoo.de>
2021-08-08 12:34:18 +0200

    tests registration

--- app/verification/mock.py
+++ app/verification/mock.py
@@ -5,10 +5,9 @@ from verification.exceptions import VerificationCodeSendException, VerificationC
 
 class MockPhoneVerificationService(PhoneVerificationService):
 
-    def __init__(self):
-        self.phone_number_cannot_send_verification_code = phonenumbers.parse('+31 6 5555 5555', None)
-        self.phone_number_verification_id = 'mock_phone_number_verification_id'
-        self.verification_code_valid = '0' * SMS_PIN_LENGTH
+    phone_number_cannot_send_verification_code = phonenumbers.parse('+31 6 5555 5555', None)
+    phone_number_verification_id = 'mock_phone_number_verification_id'
+    verification_code_valid = '0' * SMS_PIN_LENGTH
 
     async def send_verification_code(self, phone_number) -> str:
             if phonenumbers.parse(phone_number, None) == self.phone_number_cannot_send_verification_code:
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 import pytest
+from starlette import responses
 
 from main import app
 from crud import recreate_database
@@ -10,6 +11,24 @@ client = TestClient(app)
 def setup_run_teardown():
     recreate_database()
     yield
+
+class TestRegister:
+    def test_register_no_phone_number(self, setup_run_teardown):
+        response = client.post('/register')
+        # Expecting form in body
+        assert response.status_code == 422
+
+    def test_register_invalid_phone_number(self, setup_run_teardown):
+        response = client.post('/register', data={ 'phone_number' : 'not a phone number' })
+        assert response.status_code == 400
+
+    def test_register_phone_number(self, setup_run_teardown):
+        form_data = { 'phone_number' : '+31 6 5555 1111' }
+        response = client.post('/register', data=form_data)
+        assert response.status_code == 201
+        # Should not create a new user
+        response = client.post('/register', data=form_data)
+        assert response.status_code == 200
 
 class TestRefresh:
     def test_refresh_no_token(self, setup_run_teardown):
