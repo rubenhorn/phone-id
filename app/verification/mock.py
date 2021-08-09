078d4534799c24a8683cc4a10fc39b45f6b3dbe3
Ruben Horn <horn.ruben@yahoo.de>
2021-08-09 20:29:08 +0200

    test registration

--- app/verification/mock.py
+++ app/verification/mock.py
@@ -5,12 +5,12 @@ from verification.exceptions import VerificationCodeSendException, VerificationC
 
 class MockPhoneVerificationService(PhoneVerificationService):
 
-    phone_number_cannot_send_verification_code = phonenumbers.parse('+31 6 5555 5555', None)
+    phone_number_cannot_send_verification_code = '+31655555555'
     phone_number_verification_id = 'mock_phone_number_verification_id'
     verification_code_valid = '0' * SMS_PIN_LENGTH
 
     async def send_verification_code(self, phone_number) -> str:
-            if phonenumbers.parse(phone_number, None) == self.phone_number_cannot_send_verification_code:
+            if phone_number == self.phone_number_cannot_send_verification_code:
                 raise VerificationCodeSendException()
             else:
                 return self.phone_number_verification_id
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
+from app.verification.mock import MockPhoneVerificationService
 from fastapi.testclient import TestClient
 import pytest
 from starlette import responses
 
 from main import app
 from crud import recreate_database
+from verification.mock import MockPhoneVerificationService
 
 client = TestClient(app)
 
@@ -21,6 +23,10 @@ class TestRegister:
     def test_register_invalid_phone_number(self, setup_run_teardown):
         response = client.post('/register', data={ 'phone_number' : 'not a phone number' })
         assert response.status_code == 400
+
+    def test_register_unverifiable_phone_number(self, setup_run_teardown):
+        response = client.post('/register', data={ 'phone_number' : MockPhoneVerificationService.phone_number_cannot_send_verification_code })
+        assert response.status_code == 500
 
     def test_register_phone_number(self, setup_run_teardown):
         form_data = { 'phone_number' : '+31 6 5555 1111' }
